254a61e852a6d4a5ca382268a1d2677109a14c89
juan <jjsorianoe@gmail.com>
2018-01-21 20:58:00 +0100

    graphs

--- create_edges.py
+++ create_edges.py
@@ -5,23 +5,23 @@ from itertools import combinations
 import numpy as np
 from csv_helper import CSVHelper
 
-# matrix = createMatrix()
-# result = DBSCAN(matrix, 0.6, 10)
+matrix = createMatrix()
+results = DBSCAN(matrix, 0.5, 12)
 
-result = CSVHelper.load_csv("dbscan_results.csv")
-
+# result = CSVHelper.load_csv("k2.csv")
+# result = list(map(lambda x: int(float(x)), result)) 
 # noise_list = CSVHelper.load_csv("noise.csv")
 # noise_list = list(map(lambda x: int(x), noise_list)) 
 def get_edges(result):
     edges = []
     for c in result:
-        c = [int(item) for item in c if (item != ',' and item != ' ')]
+        # c = [int(item) for item in c if (item != ',' and item != ' ')]
         comb = combinations(c, 2)
         # Print the obtained combinations
         for i in list(comb):
             edges.append(i)
     return edges
-edges = get_edges(result)
+edges = get_edges(results)
 
-np.savetxt("edges_dbscan.csv", edges, fmt='%i',delimiter=",")
+np.savetxt("edges_2.csv", edges, fmt='%i',delimiter=",")
 
