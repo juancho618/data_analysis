c3ebef5999e9964fcb7da9c3c22996a5be67f18a
Juan <jjsorianoe@gmail.com>
2018-01-21 11:17:44 +0100

    edges

--- create_edges.py
+++ create_edges.py
@@ -1,10 +1,20 @@
 from clustering import *
+# A Python program to print all 
+# combinations of given length
+from itertools import combinations
+import numpy as np
 
 edges = []
 matrix = createMatrix()
 result = DBSCAN(matrix, 0.6, 10)
 
 for c in result:
-    for n in c:
-        
+    # Get all combinations of [1, 2, 3]
+    # and length 2
+    comb = combinations(c, 2)
+    # Print the obtained combinations
+    for i in list(comb):
+        edges.append(i)
+
+np.savetxt("edges.csv", edges, fmt='%i',delimiter=",")
 
